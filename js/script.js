ab3db9e26cce2432b830913551b00b853a9f722e
Hitesh <hitesh.hirpara@gmail.com>
2018-09-01 23:02:05 +0530

    intial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+	$(".scroll").click(function (event) {
+ 		event.preventDefault();
+ 		$('html,body').animate({
+ 			scrollTop: $(this.hash).offset().top
+ 		}, 1000);
+ 	});
+ 	$("#owl-demo").owlCarousel({
+ 		items: 1,
+ 		lazyLoad: true,
+ 		autoPlay: true,
+ 		navigation:false,
+ 		navigationText: true,
+ 		pagination: true,
+ 	});
+});
